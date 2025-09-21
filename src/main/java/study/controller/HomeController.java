656bdc80a50c8cafeb025e78ad939629929611c5
深川　貴司 <fukagawatakashiee893@gmail.com>
2025-09-21 12:19:00 +0900

    トップの追加

--- src/main/java/study/controller/HomeController.java
+++ src/main/java/study/controller/HomeController.java
@@ -4,9 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HelloController {
-	@GetMapping("/hello")
-	public String hello() {
-		return "hello";
+public class HomeController {
+	@GetMapping("/")
+	public String home() {
+		return "index";
 	}
 }
