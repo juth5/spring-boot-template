6a2e350ccd5862ec62c7276269b9445d7038d1d4
深川　貴司 <fukagawatakashiee893@gmail.com>
2025-11-08 12:30:29 +0900

    entity

--- src/main/java/study/model/Account.java
+++ src/main/java/study/model/Account.java
@@ -1,5 +1,20 @@
 package study.model;
 
+import java.time.OffsetDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Account {
-  
+    private Long id;
+    private String username;
+    private String password;
+    private String role;
+    private OffsetDateTime createdAt;
 }
