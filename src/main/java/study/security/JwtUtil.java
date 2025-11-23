c283b95066be471a5e52cf6a43326de77e0e27bd
深川　貴司 <fukagawatakashiee893@gmail.com>
2025-11-23 15:58:10 +0900

    秘密キーを定義

--- src/main/java/study/security/JwtUtil.java
+++ src/main/java/study/security/JwtUtil.java
@@ -3,18 +3,26 @@ package study.security;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
 
 import java.security.Key;
 import java.util.Date;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class JwtUtil {
 
-    // ✅ 学習用：起動するたびに作られる鍵（後で固定化に変更）
-    private final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    @Value("${JWT_SECRET}")
+    private String secret;
 
+    private Key key;
+
+    @PostConstruct
+    public void init() {
+        this.key = Keys.hmacShaKeyFor(secret.getBytes());
+    }
     // ✅ 有効期限（例：1日）
     private final long EXPIRATION = 1000 * 60 * 60 * 24;
 
