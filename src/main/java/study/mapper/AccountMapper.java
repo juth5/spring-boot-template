efa90c1a9115a99f1b9db3bbc1839d08c39eb942
juth <63380489+juth5@users.noreply.github.com>
2025-11-23 16:46:48 +0900

    Revert "account entity"

--- src/main/java/study/mapper/AccountMapper.java
+++ src/main/java/study/mapper/AccountMapper.java
@@ -1,20 +1,5 @@
 package study.mapper;
 
-import java.util.List;
-
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-
-import study.model.Account;
-
-@Mapper
-public interface AccountMapper {
-    //レコードを1件追加
-    void insertAccount(Account account);
-    //ユーザー名から取得
-    Account findByUsername(String username);
-    //ユーザーidから取得
-    Account findByUserId(@Param("userId") Long userId);
-    //全件取得
-    List<Account> getAccounts();
+public class AccountMapper {
+  
 }
